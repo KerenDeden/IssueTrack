05734c5c55bfa6628a10369313f665d216bcc0c5
Sourav Gain <souravgain605@gmail.com>
2022-10-06 20:15:42 +0530

    Added Priority & Modified Severity

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function submitIssue(e) {
   const getInputValue = (id) => document.getElementById(id).value;
   const description = getInputValue("issueDescription");
+  const priority = getInputValue("issuePriority");
   const severity = getInputValue("issueSeverity");
   const assignedTo = getInputValue("issueAssignedTo");
   const id = Math.floor(Math.random() * 100000000) + "";
@@ -19,7 +20,7 @@ function submitIssue(e) {
     document
       .getElementById("add-issue")
       .removeAttribute("data-target", "#emptyField");
-    const issue = { id, description, severity, assignedTo, status };
+    const issue = { id, description, priority, severity, assignedTo, status };
     let issues = [];
     if (localStorage.getItem("issues")) {
       issues = JSON.parse(localStorage.getItem("issues"));
@@ -53,12 +54,14 @@ const fetchIssues = () => {
   issuesList.innerHTML = "";
 
   for (var i = 0; i < issues.length; i++) {
-    const { id, description, severity, assignedTo, status } = issues[i];
+    const { id, description, priority, severity, assignedTo, status } =
+      issues[i];
 
     issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-signal"></span> ${priority}</p>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
                               <button onclick="closeIssue(${id})" class="btn btn-warning">Close</button>
